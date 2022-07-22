f14de39da476328423aec5e0f2349d534e99576e
igentuman <igentuman@gmail.com>
2022-07-23 00:06:24 +0300

    Forge hammer JEI recipe category

--- src/main/java/igentuman/dveins/integration/jei/JEIPlugin.java
+++ src/main/java/igentuman/dveins/integration/jei/JEIPlugin.java
@@ -1,14 +1,18 @@
 package igentuman.dveins.integration.jei;
 
+import igentuman.dveins.RegistryHandler;
+import igentuman.dveins.common.recipe.ForgeHammerRecipe;
+import igentuman.dveins.common.recipe.RecipeHandler;
 import igentuman.dveins.util.ItemHelper;
 import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.recipe.IRecipeCategoryRegistration;
+import net.minecraft.item.ItemStack;
 
 import static igentuman.dveins.DVeins.MODID;
 
-//@mezz.jei.api.JEIPlugin
+@mezz.jei.api.JEIPlugin
 public class JEIPlugin implements IModPlugin {
     public JEIPlugin() {
         super();
@@ -16,15 +20,15 @@ public class JEIPlugin implements IModPlugin {
 
     @Override
     public void register(IModRegistry registry) {
-        //registry.handleRecipes(ForgeHammerRecipe.class, DrillRecipeCategory.Wrapper::new, MODID+"_drill");
-        //registry.addRecipeCatalyst(ItemHelper.getStackFromString("dveins:drill",0),MODID+"_drill");
-        //registry.addRecipes(EvTweaksRecipes.FORGE_HAMMER.getAll(), MODID+"_drill");
+        registry.handleRecipes(ForgeHammerRecipe.class, ForgeHammerRecipeCategory.Wrapper::new, MODID+"_forgehammer");
+        registry.addRecipeCatalyst(new ItemStack(RegistryHandler.FORGE_HAMMER),MODID+"_forgehammer");
+        registry.addRecipes(RecipeHandler.FORGE_HAMMER.getAll(), MODID+"_forgehammer");
     }
 
     @Override
     public void registerCategories(IRecipeCategoryRegistration registry) {
         final IGuiHelper guiHelper = registry.getJeiHelpers().getGuiHelper();
-      //  registry.addRecipeCategories(new DrillRecipeCategory(guiHelper));
+        registry.addRecipeCategories(new ForgeHammerRecipeCategory(guiHelper));
     }
 
 }
